e4918524fcd252c881be364b61892a0e63b019ae
msebbane <msebbane@Transcendance.myguest.virtualbox.org>
2023-10-05 23:15:41 +0200

    nouvelle page home done [FRONT]

--- frontend/src/pages/Home2.tsx
+++ frontend/src/pages/Home2.tsx
@@ -5,27 +5,24 @@ import { Link } from 'react-router-dom'; // Importez Link depuis React Router
 function Home2() {
   return (
     <div>
-      <div className="container-grid">
-        <Link to="/play" className="container container-1">
-          Play pongGame
-        </Link>
-        <Link to="/profile" className="container container-1">
-          Profile
-        </Link>
-        <Link to="/chat" className="container container-1">
-          Chat
-        </Link>
-	<Link to="/leaderboard" className="container container-1">
-          LeaderBoard
-        </Link>
-        <Link to="/watch" className="container container-1">
-          Watch Game
-        </Link>
-        <Link to="/settings" className="container container-1">
-          Settings
+     <div>
+	<Link to="/play" className="centered-container blinking-text">
+          PLAY
         </Link>
       </div>
+      <div className="container-grid">
+        <Link to="/leaderboard" className="container container-1">
+          Leader
+	  Board
+        </Link>
+        <Link to="/watch" className="container container-2">
+          Watch Game
+        </Link>
+        <Link to="/chat" className="container container-3">
+          Chat
+        </Link>
     </div>
+</div>
   );
 }
 
